4e5f8fbad8f0d99ed422eea11eb9966c75153263
eznoamd <enzoamd6@gmail.com>
2026-03-26 13:06:02 -0300

    Revert "test: testando se o discord bot esta funcionando"
    This reverts commit 909f8f254a15239bde8a87e29e7bf907e107b5cf.

--- main.py
+++ main.py
@@ -131,7 +131,7 @@ tomorrow = now + timedelta(1)
 
 
 if args.token and args.channel:
-    if now.strftime('%a') in ('Sun', 'Wed', 'Fri'):
+    if now.strftime('%a') in ('Sun', 'Wed'):
         reminder = "📣 Agende o RU !!!! @everyone"
         send_discord_reminder(args.token, args.channel, reminder)
 
